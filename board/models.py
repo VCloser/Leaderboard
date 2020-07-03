cd1d4ebfd3e0e5ea03f4bfec6309b87a7a4274bd
leiyuhang <1660865699@qq.comm>
2020-07-03 16:14:18 +0800

    feat(board): create modell

--- board/models.py
+++ board/models.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding=utf-8 -*-
+
+
 from django.db import models
 
-# Create your models here.
+
+class LeaderBoard(models.Model):
+    '''
+    玩家分数排行榜表
+    '''
+    client_num = models.IntegerField(verbose_name='客户端号')
+    score = models.PositiveIntegerField(verbose_name='分数')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
